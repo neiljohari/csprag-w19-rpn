efd449441f0edc9c658014a2214322c455e97542
Neil Johari <neil@johari.tech>
2019-02-27 23:26:37 -0500

    Add test for exponentiation operator

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,6 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exponentiation(self):
+        result = rpn.calculate("3 3 ^")
+        self.assertEqual(27, result)
